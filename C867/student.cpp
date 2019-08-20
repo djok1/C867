6b9c5a5080ce088aaf1c596974706a386b1bbafa
djok1 <djok@live.com>
2019-08-20 12:01:21 -0600

    Finished getters and setters for student

--- C867/student.cpp
+++ C867/student.cpp
@@ -30,10 +30,77 @@ class student
 		{
 			firstName = FirstName;
 		}
+
+		void setlastName(string LastName)
+		{
+			lastName = LastName;
+		}
+
+		void setemail(string Email)
+		{
+			email = Email;
+		}
+
+		void setage(int Age)
+		{
+			age = Age;
+		}
+
+		void setdaysInCourse(int Days0,int Days1,int Days2)
+		{
+			if (Days0 > 0) 
+			{
+				daysInCourse[0] = Days0;
+			}
+			if (Days1 > 0)
+			{
+				daysInCourse[1] = Days1;
+			}
+			if (Days2 > 0)
+			{
+				daysInCourse[2] = Days2;
+			}
+		}
+
+		void setdegreeType(DegreeType Degree)
+		{
+			degreeType = Degree;
+		}
+
 		//getters
 		string getstudentID()
 		{
 			return studentID;
+		}
+
+		string getfirstName()
+		{
+			return firstName;
+		}
+
+		string getlastName()
+		{
+			return lastName;
+		}
+
+		string getemail()
+		{
+			return email;
+		}
+
+		int getage()
+		{
+			return age;
+		}
+
+		int getdaysInCourse(int I)
+		{
+			return daysInCourse[I];
+		}
+
+		DegreeType getdegreeType()
+		{
+			return degreeType;
 		}
 
 };
